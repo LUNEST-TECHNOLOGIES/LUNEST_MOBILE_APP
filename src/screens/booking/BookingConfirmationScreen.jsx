7c0da804bb3563b75f649ea92e1c9fcdd84cb9a1
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2026-04-29 22:03:55 +0100

    good update

--- src/screens/booking/BookingConfirmationScreen.jsx
+++ src/screens/booking/BookingConfirmationScreen.jsx
@@ -2361,6 +2361,7 @@ const BookingConfirmationScreen = () => {
         onDownloadReceipt={handleReceiptDownload}
         onDownloadAgreement={handleAgreementDownload}
         loading={isDownloading}
+        showAgreement={["confirmed", "ongoing", "completed"].includes(statusLower)}
       />
 
       <DownloadConfirmationModal
